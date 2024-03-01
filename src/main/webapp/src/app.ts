805fb70b59e93b78498353a3ff474b0b44cdb9a2
Stefan Arisona <robot@arisona.ch>
2024-03-01 12:51:12 +0800

    Modernize api code

--- src/main/webapp/src/app.ts
+++ src/main/webapp/src/app.ts
@@ -6,58 +6,51 @@ const resetId = 'nova-reset';
 const reloadId = 'nova-reload';
 const selectedContent = 'nova-content';
 
-document.addEventListener('DOMContentLoaded', function () {
-    // fetch state from server and set ui elements
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', '/api/get-state');
-    xhr.send();
-    xhr.onload = function () {
-        if (xhr.status != 200) {
-            console.warn(`get-state: ${xhr.status}: ${xhr.statusText}`);
-            return;
-        }
-        try {
-            const response = JSON.parse(xhr.response);
+syncServerState();
+addListeners();
+
+function syncServerState(): void {
+    fetch('/api/get-state')
+        .then(response => {
+            if (!response.ok)
+                throw new Error(response.status.toString());
+            return response;
+        })
+        .then(response => response.json())
+        .then(data => {
             rangeIds.forEach(id => {
                 const range = document.getElementById(id) as HTMLInputElement;
-                range.value = response[id];
+                range.value = data[id];
             });
             const contents = document.getElementById(contentsId) as HTMLSelectElement;
-            for (let i = 0; i < response[contentsId].length; ++i) {
-                contents.appendChild(new Option(response[contentsId][i], i.toString(), false, i == response[selectedContent]));
+            for (let i = 0; i < data[contentsId].length; ++i) {
+                contents.appendChild(new Option(data[contentsId][i], i.toString(), false, i == data[selectedContent]));
             }
-        } catch (e) {
-            console.error(e);
-        }
-    };
+        })
+        .catch(function (error) {
+            console.error('Request failed ', error)
+        });
+}
 
+function addListeners(): void {
     // add event listeners to ui elements
     document.getElementById(contentsId)!.addEventListener('change', event => {
         const target = event.target as HTMLSelectElement;
-        call(target.id, target.value);
+        fetch(`/api/${target.id}?value=${target.value}`);
     });
 
     rangeIds.forEach(id => document.getElementById(id)!.addEventListener('input', event => {
         const target = event.target as HTMLInputElement;
-        call(target.id, target.value);
+        fetch(`/api/${target.id}?value=${target.value}`);
     }));
 
     document.getElementById(resetId)!.addEventListener('click', event => {
         const target = event.target as HTMLButtonElement;
-        call(target.id);
+        fetch(`/api/${target.id}`);
     });
 
     document.getElementById(reloadId)!.addEventListener('click', event => {
         const target = event.target as HTMLButtonElement;
-        call(target.id);
+        fetch(`/api/${target.id}`);
     });
-});
-
-function call(id : string, value : string | number | undefined = undefined) {
-    const xhr = new XMLHttpRequest();
-    if (value)
-        xhr.open('GET', `/api/${id}?value=${value}`);
-    else
-        xhr.open('GET', `/api/${id}`);
-    xhr.send();
 }
